5747ee2a28b5095388cb04e8fbbe3f8b1d81ef7e
delmetskellogg <david.elmets@kellogg.northwestern.edu>
2022-02-21 04:05:04 +0000

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :Places
-  
   resources :Posts   
   # Defines the root path route ("/")
   get '/places/:id', to: 'places#show'
   get '/posts/:id', to: 'posts#show'
 
   # root "articles#index"
-  root to: "main#index"
+  root to: "places#index"
 end
