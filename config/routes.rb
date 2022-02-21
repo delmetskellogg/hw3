7ebf2cb76e65a529bc049d8542d80a3c6b2fb365
delmetskellogg <david.elmets@kellogg.northwestern.edu>
2022-02-21 03:56:03 +0000

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   
   resources :Posts   
   # Defines the root path route ("/")
+  get '/places/:id', to: 'places#show'
+  get '/posts/:id', to: 'posts#show'
+
   # root "articles#index"
+  root to: "main#index"
 end
